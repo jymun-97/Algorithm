20b8175e8b17a2f76afaac60dbb4833508569055
jymun-97 <mjy0750@gmail.com>
2021-08-20 20:19:19 +0900

    add: solution of Boj_1181

---
+++ Baekjun/1. Sort/단어 정렬 1181/Boj_1181.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Word implements Comparable<Word> {
+    String word;
+    int length;
+    
+    public Word(String word) {
+        this.word = word;
+        length = word.length();
+    }
+
+    public int compareTo(Word other) {
+        if (length == other.length) {
+            return word.compareTo(other.word);
+        }
+        return length - other.length;
+    }
+}
+
+
+public class Boj_1181 {
+    static int n;
+    static ArrayList<Word> words = new ArrayList<>();
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            words.add(new Word(br.readLine()));
+        }
+    }
+
+    static void solve() {
+        Collections.sort(words);
+        String preWord = "";
+
+        StringBuilder sb = new StringBuilder();
+        for (Word word : words) {
+            if (!word.word.equals(preWord))
+                sb.append(word.word).append('\n');
+            
+            preWord = word.word;
+        }
+
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
