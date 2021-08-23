8a57f9df62950aad14ebfde90c07205c330ceffd
jymun-97 <mjy0750@gmail.com>
2021-08-23 20:19:19 +0900

    add: solution of Boj_21919

--- Java/Baekjun/10. Review/21919 - 소수 최소 공배수/Main.java
+++ Java/Baekjun/10. Review/21919 - 소수 최소 공배수/Main.java
@@ -17,7 +17,6 @@ public class Main {
         for (int i = 0; i < n; i++) nums[i] = Integer.parseInt(st.nextToken());
 
     }
-
     static void solve() {
         for (int num : nums) {
             if (isPrime(num)) {
