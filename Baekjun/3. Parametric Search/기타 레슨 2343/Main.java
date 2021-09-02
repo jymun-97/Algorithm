a4b8da770be107736385425d82b7977852d8a4b3
jymun-97 <mjy0750@gmail.com>
2021-09-02 20:19:19 +0900

    add: solution of Boj_2805

---
+++ Baekjun/3. Parametric Search/기타 레슨 2343/Main.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] lectures;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        lectures = new int[N];
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            lectures[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static boolean isValuable(int size) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            if (sum + lectures[i] >= size) {
+                count++;
+                sum = lectures[i];
+            }
+            else {
+                sum += lectures[i];
+            }
+        }
+        return count >= M;
+    }
+
+    static void solve() {
+        int left = lectures[0];
+        for (int lecture : lectures) {
+            if (left < lecture) left = lecture;
+        }
+        int right = 1000000000;
+        int answer = left;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isValuable(mid)) {
+                answer = mid;
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
---
+++ Baekjun/3. Parametric Search/나무 자르기 2805/Main.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] heights;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        heights = new int[n];
+        m = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            heights[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static boolean isValuable(int height) {
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (heights[i] > height) {
+                sum += heights[i] - height;
+            }
+        }
+        return sum >= m;
+    }
+
+    static void find() {
+        int answer = 0;
+        int left = 0;
+        int right = 2000000000;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isValuable(mid)) {
+                left = mid + 1;
+                answer = mid;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        find();
+    }
+}
