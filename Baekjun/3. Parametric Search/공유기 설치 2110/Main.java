2757087dc7b7c4f8f06f3257feb8d76e6101650e
jymun-97 <mjy0750@gmail.com>
2021-09-02 20:19:19 +0900

    add: solution of Boj_2110

---
+++ Baekjun/3. Parametric Search/공유기 설치 2110/Main.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, c;
+    static int[] locations;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        locations = new int[n];
+
+        c = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            locations[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(locations);
+    }
+
+    static boolean isValuable(int distance) {
+        int count = 1;
+        int last = locations[0];
+
+        for (int i = 1; i < n; i++) {
+            if (distance <= locations[i] - last) {
+                count++;
+                last = locations[i];
+            }
+        }
+        return count >= c;
+    }
+
+    static void solve() {
+        int left = 0;
+        int right = locations[n - 1];
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isValuable(mid)) {
+                answer = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
