055a83925232aed44695caa8d03f3288e3e83946
jymun-97 <mjy0750@gmail.com>
2021-09-06 20:19:19 +0900

    add: solution of Boj_20364_부동산 다툼

--- Java/Baekjun/6. Tree/20364 - 부동산 다툼/Main.java
+++ Java/Baekjun/6. Tree/20364 - 부동산 다툼/Main.java
@@ -10,7 +10,6 @@ public class Main {
     static boolean[] visit;
     static StringBuilder sb = new StringBuilder();
 
-
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
