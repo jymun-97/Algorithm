7b7c8413034d06a1adf39391e501fa9da7c52024
jymun-97 <mjy0750@gmail.com>
2021-09-07 20:19:19 +0900

    add: solution of Boj_1253

---
+++ Baekjun/4. Two Pointer/1253 - 좋다/Main.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] nums;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+    }
+
+    static void solve() {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int target = nums[i];
+
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                if (left == i) {
+                    left++; 
+                    continue;
+                }
+                if (right == i) {
+                    right--;
+                    continue;
+                }
+
+                int sum = nums[left] + nums[right];
+                if (sum > target) right--;
+                else if (sum < target) left++;
+                else {
+                    count++;
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
