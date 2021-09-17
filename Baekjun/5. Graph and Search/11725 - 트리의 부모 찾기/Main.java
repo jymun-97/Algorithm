5629d431191b56fbb754a2be68ec798462038a73
jymun-97 <mjy0750@gmail.com>
2021-09-17 20:19:19 +0900

    add: solution of Boj_11725

---
+++ Baekjun/5. Graph and Search/11725 - 트리의 부모 찾기/Main.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static boolean[] visit;
+    static ArrayList<Integer>[] graph;
+    static int[] parentOf;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visit = new boolean[n + 1];
+        graph = new ArrayList[n + 1];
+        parentOf = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 1; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph[from].add(to);
+            graph[to].add(from);
+        }
+    }
+
+    static void solve() {
+        int root = 1;
+        dfs(root);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= n; i++) {
+            sb.append(parentOf[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int from) {
+        visit[from] = true;
+
+        for (int to : graph[from]) {
+            if (!visit[to]) {
+                parentOf[to] = from;
+                dfs(to);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
