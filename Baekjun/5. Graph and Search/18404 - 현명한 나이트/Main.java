3ab9d14333cf8b73331d4615e22b2ba403759aa7
jymun-97 <mjy0750@gmail.com>
2021-09-30 20:19:19 +0900

    add: solution of Boj_18404

---
+++ Baekjun/5. Graph and Search/18404 - 현명한 나이트/Main.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int size, nEnemies;
+    static int startR, startC;
+    static int[][] targets;
+    static int[][] dist;
+    static int[][] dir = { {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2} };
+    
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        size = Integer.parseInt(st.nextToken());
+        nEnemies = Integer.parseInt(st.nextToken());
+
+        targets = new int[nEnemies][2];
+        dist = new int[size + 1][size + 1];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                dist[i][j] = -1;
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        startR = Integer.parseInt(st.nextToken());
+        startC = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < nEnemies; i++) {
+            st = new StringTokenizer(br.readLine());
+            targets[i][0] = Integer.parseInt(st.nextToken());
+            targets[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void solve() {
+        bfs();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < targets.length; i++) {
+            sb.append(dist[targets[i][0]][targets[i][1]]).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+
+    static void bfs() {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(startR);
+        que.add(startC);
+        dist[startR][startC] = 0;
+
+        while (!que.isEmpty()) {
+            int row = que.poll();
+            int col = que.poll();
+
+            for (int i = 0; i < dir.length; i++) {
+                int nr = row + dir[i][0];
+                int nc = col + dir[i][1];
+
+                if (nr <= 0 || nr > size || nc <= 0 || nc > size) continue;
+                if (dist[nr][nc] < 0) {
+                    que.add(nr);
+                    que.add(nc);
+                    dist[nr][nc] = dist[row][col] + 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
---
+++ Baekjun/6. Tree/5639 - 이분 검색 트리/Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int[][] graph;
+    static int root;
+    static Queue<Integer> que = new LinkedList<>();
+    static StringBuilder sb = new StringBuilder();
+    
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        graph = new int[1000000][2];
+        String line;
+        while (true) {
+            line = br.readLine();
+            if (line == null || line.equals("")) break;
+            
+            que.add(Integer.parseInt(line));
+        }
+    }
+
+    static void solve() {
+        root = que.poll();
+        while (!que.isEmpty()) {
+            int node = que.poll();
+            dfs(root, node);
+        }
+        postOrder(root);
+
+        System.out.println(sb);
+    }
+    
+    static void dfs(int parent, int child) {
+        if (parent > child) {
+            if (graph[parent][0] == 0) graph[parent][0] = child;
+            else dfs(graph[parent][0], child);
+        }
+        else {
+            if (graph[parent][1] == 0) graph[parent][1] = child;
+            else dfs(graph[parent][1], child);
+        }
+    }
+
+    static void postOrder(int parent) {
+        if (parent == 0) return;
+        
+        postOrder(graph[parent][0]);
+        postOrder(graph[parent][1]);
+        sb.append(parent).append('\n');
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        input();
+        solve();
+    }
+}
