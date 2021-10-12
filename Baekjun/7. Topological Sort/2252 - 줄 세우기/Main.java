c4a379e29aefa7d11888bfb0d1643b0002dc39ab
jymun-97 <mjy0750@gmail.com>
2021-10-12 20:19:19 +0900

    add: solution of Boj_2252

---
+++ Baekjun/7. Topological Sort/2252 - 줄 세우기/Main.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] indeg;
+    static ArrayList<Integer>[] graph;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        indeg = new int[n + 1];
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph[from].add(to);
+            indeg[to]++;
+        }
+    }
+
+    static void solve() {
+        Queue<Integer> que = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            if (indeg[i] == 0) que.add(i);
+        }
+
+        while (!que.isEmpty()) {
+            int from = que.poll();
+            sb.append(from).append(' ');
+
+            for (int to : graph[from]) {
+                indeg[to]--;
+                if (indeg[to] == 0) que.add(to);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
