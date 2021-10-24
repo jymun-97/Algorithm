cd5f741de95cb3fe4df85db056a9fa7d75a61f16
jymun-97 <mjy0750@gmail.com>
2021-10-24 20:19:19 +0900

    add: solution of Boj_10870

---
+++ Baekjun/9. Dynamic Programming/10870 - 피보나치 수 5/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n;
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+    }
+
+    static void solve() {
+        int[] dy = new int[21];
+        dy[0] = 0;
+        dy[1] = 1;
+
+        for (int i = 2; i < 21; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
+        }
+        System.out.println(dy[n]);
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+}
