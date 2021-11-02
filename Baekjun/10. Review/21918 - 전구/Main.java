e86255f1d10e9a6f1e9fd9fc3546f744f33235e3
jymun-97 <mjy0750@gmail.com>
2021-11-02 20:19:19 +0900

    add: solution of Boj_21918

---
+++ Baekjun/10. Review/21918 - 전구/Main.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] a;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        a = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) a[i] = Integer.parseInt(st.nextToken());
+    
+        while (m --> 0) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            switch (command) {
+                case 1:
+                    func1(x, y);
+                    break;
+                case 2:
+                    func2(x, y);
+                    break;
+                case 3:
+                    func3(x, y);
+                    break;
+                default:
+                    func4(x, y);
+                    break;
+            }
+        }
+    }
+
+    static void func1(int i, int x) {
+        a[i] = x;
+    }
+
+    static void func2(int l, int r) {
+        for (int i = l; i <= r; i++) {
+            a[i] = 1 - a[i];
+        }
+    }
+
+    static void func3(int l, int r) {
+        for (int i = l; i <= r; i++) {
+            a[i] = 0;
+        }
+    }
+
+    static void func4(int l, int r) {
+        for (int i = l; i <= r; i++) {
+            a[i] = 1;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        input();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) sb.append(a[i]).append(' ');
+        System.out.println(sb);
+    }
+}
