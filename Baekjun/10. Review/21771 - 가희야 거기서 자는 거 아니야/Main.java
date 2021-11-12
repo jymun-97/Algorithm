205e8d648d871c9104396c4107d64415b4be2c9c
jymun-97 <mjy0750@gmail.com>
2021-11-12 20:19:19 +0900

    add: solution of Boj_21771

---
+++ Baekjun/10. Review/21771 - 가희야 거기서 자는 거 아니야/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int maxP, pCount;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        st.nextToken(); st.nextToken();
+        maxP = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < row; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < col; j++) {
+                if (line.charAt(j) == 'P') pCount++;
+            }
+        }
+    }
+
+    static void solve() {  
+        int result = 0; 
+        if (pCount < maxP) result = 1;
+
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        input();
+        solve();
+    }
+}
