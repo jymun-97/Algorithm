a95c98ba568154b55ebf9b82812f71f3a33c5278
jymun-97 <mjy0750@gmail.com>
2021-11-13 20:19:19 +0900

    add: solution of Boj_21773

---
+++ Baekjun/10. Review/21773 - 가희와 프로세스 1/Main.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+class Process implements Comparable<Process> {
+    int id, remain, priority;
+    public Process(int i, int r, int p) {
+        id = i;
+        remain = r;
+        priority = p;
+    } 
+    public void run() {
+        remain--;
+        priority--;
+    }
+
+    public int compareTo(Process p) {
+        if (priority != p.priority) return p.priority - priority;
+        else return id - p.id;
+    }
+}
+
+public class Main {
+
+    static int n, t;
+    static PriorityQueue<Process> que = new PriorityQueue<>();
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        t = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        while (n --> 0) {
+            st = new StringTokenizer(br.readLine());
+            int id = Integer.parseInt(st.nextToken());
+            int remain = Integer.parseInt(st.nextToken());
+            int priority = Integer.parseInt(st.nextToken());
+
+            que.add(new Process(id, remain, priority));
+        }
+    }
+
+    static void solve() {
+        StringBuilder sb = new StringBuilder();
+
+        int time = 1;
+        while (time <= t && !que.isEmpty()) {
+            Process process = que.poll();
+            process.run();
+            sb.append(process.id).append('\n');
+
+            if (process.remain != 0) que.add(process);
+            time++;
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        input();
+        solve();
+    }
+}
