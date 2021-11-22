8bf1e8ce6cbbba3a87c9570b124653a48a3b71f4
jymun-97 <mjy0750@gmail.com>
2021-11-22 20:19:19 +0900

    add: Level 2 - 위장

---
+++ Programmers/1. Hash/Level 2 - 위장/App.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        return answer - 1;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
+
+// 13m
