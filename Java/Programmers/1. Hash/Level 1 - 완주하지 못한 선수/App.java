834f63dbb10400902e73983d80c7ab2d9f73f7b3
jymun-97 <mjy0750@gmail.com>
2021-11-25 20:19:19 +0900

    add: solution of Pro-완주하지 못한 선수

--- Java/Programmers/1. Hash/Level 1 - 완주하지 못한 선수/App.java
+++ Java/Programmers/1. Hash/Level 1 - 완주하지 못한 선수/App.java
@@ -23,10 +23,3 @@ class Solution {
         return answer;
     }
 }
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-
-// 15m
