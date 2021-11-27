004a9ddb369c4c5a56c21c79e98569ab9e1e3ebe
jymun-97 <mjy0750@gmail.com>
2021-11-27 20:19:19 +0900

    add: solution of Programmers K번째수

---
+++ Programmers/4. Sort/Level 1 - K번째수/App.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+
+            for (int idx = commands[i][0] - 1; idx < commands[i][1]; idx++)
+                list.add(array[idx]);
+
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1);
+        }
+
+        return answer;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
