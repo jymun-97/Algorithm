2ef0de98279f7663d6ea87778dbd64f9f8c271a8
jymun-97 <mjy0750@gmail.com>
2021-11-27 20:19:19 +0900

    add: solution of Programmers 가장 큰 수

---
+++ Programmers/4. Sort/Level 2 - 가장 큰 수/App.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+
+    public String solution(int[] numbers) {
+        String answer = "";
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int num : numbers) {
+            list.add(Integer.toString(num));
+        }
+        Collections.sort(list, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+
+        for (String num : list) answer += num;
+
+        if (answer.charAt(0) == '0') return "0";
+        return answer;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution s = new Solution();
+        int[] numbers = new int[] {10,101010,0,2};
+
+        System.out.println(s.solution(numbers));
+    }
+}
