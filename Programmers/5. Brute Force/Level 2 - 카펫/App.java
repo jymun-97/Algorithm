5b4c645144cb1374484905d681574edbdfa8d567
jymun-97 <mjy0750@gmail.com>
2021-11-28 20:19:19 +0900

    add: solution of Programmers 카펫

---
+++ Programmers/5. Brute Force/Level 2 - 카펫/App.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        for (int h = 1; h <= Math.sqrt(yellow); h++) {
+            if (yellow % h != 0) continue;
+            int w = yellow / h;
+            if ((h + w + 2) * 2 == brown) {
+                answer[0] = w + 2;
+                answer[1] = h + 2;
+                break;
+            }
+        }
+        return answer;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution s = new Solution();
+        int brown = 16;
+        int yellow = 9;
+
+        int[] answer = s.solution(brown, yellow);
+        System.out.println(Arrays.toString(answer));
+    }
+}
