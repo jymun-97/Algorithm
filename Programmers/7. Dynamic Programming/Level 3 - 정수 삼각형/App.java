b43967c4100860dc8bf4c964493300c26c0ba4c3
jymun-97 <mjy0750@gmail.com>
2021-12-03 20:19:19 +0900

    add: solution of Programmers 정수 삼각형

---
+++ Programmers/7. Dynamic Programming/Level 3 - 정수 삼각형/App.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] triangle) {
+
+        int answer = 0;
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
+
+        dp[0][0] = triangle[0][0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) dp[i][j] = dp[i - 1][j];
+                else if (j == i) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
+
+                dp[i][j] += triangle[i][j];
+                if (dp[i][j] > answer) answer = dp[i][j];
+            }
+        }
+        return answer;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
