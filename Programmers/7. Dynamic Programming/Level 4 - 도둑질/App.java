b3ce0e883e54d5b7be8c408f38e5f9848f60d4c3
jymun-97 <mjy0750@gmail.com>
2021-12-06 20:19:19 +0900

    add: solution of Programmers 도둑질

---
+++ Programmers/7. Dynamic Programming/Level 4 - 도둑질/App.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int[] money) {
+
+        int n = money.length;
+        int[] dp1 = new int[n]; 
+        int[] dp2 = new int[n]; 
+
+        dp1[0] = money[0];
+        dp1[1] = Math.max(money[0], money[1]);
+        
+        dp2[0] = 0;
+        dp2[1] = money[1];
+
+        for (int i = 2; i < n - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
+        }
+
+        dp1[n - 1] = dp1[n - 2];
+        dp2[n - 1] = Math.max(dp2[n - 2], dp2[n - 3] + money[n - 1]);
+
+        return Math.max(dp1[n - 1], dp2[n - 1]);
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution s = new Solution();
+
+        int[] money = {1,2,3,1};
+        
+        System.out.println(s.solution(money));
+    }
+}
