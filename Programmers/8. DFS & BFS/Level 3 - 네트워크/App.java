cd653ee4e685ec45d3f30b042cd50dbc0b5dc85a
jymun-97 <mjy0750@gmail.com>
2021-12-08 20:19:19 +0900

    add: solution of Programmers 네트워크

---
+++ Programmers/8. DFS & BFS/Level 3 - 네트워크/App.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+class Solution {
+
+    ArrayList<Integer>[] graph;
+    boolean[] visit;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visit = new boolean[n];
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int from = i;
+            for (int to = 0; to < n; to++) {
+                if (computers[from][to] == 1 && from != to) {
+                    graph[from].add(to);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                dfs(i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public void dfs(int from) {
+        visit[from] = true;
+        for (int to : graph[from]) {
+            if (!visit[to]) {
+                dfs(to);
+            }
+        }
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
