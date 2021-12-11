9156860a363366f4815d1a79498f96273087a4f1
jymun-97 <mjy0750@gmail.com>
2021-12-11 20:19:19 +0900

    add: solution of Programmers 순위

---
+++ Programmers/10. Graph/Level 3 - 순위/App.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        boolean[][] connect = new boolean[n + 1][n + 1];
+
+        for (int[] result : results) {
+            connect[result[0]][result[1]] = true;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (connect[i][k] && connect[k][j]) {
+                        connect[i][j] = true;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= n; j++) {
+                if (connect[i][j] || connect[j][i]) count++;
+            }
+            if (count == n - 1) answer++;
+        }
+        return answer;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution s = new Solution();
+
+        int n = 5;
+        int[][] results = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
+
+        System.out.println(s.solution(n, results));
+    }
+}
