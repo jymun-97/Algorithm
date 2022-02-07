fff737bfd2eb6b905c477c6fd615846a40a602ce
jymun-97 <mjy0750@gmail.com>
2022-02-07 20:19:19 +0900

    add: solution of Boj_1806_부분합

--- Java/Baekjun/4. Two Pointer/1806 - 부분합/Main.java
+++ Java/Baekjun/4. Two Pointer/1806 - 부분합/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
         nums = new int[n];
         st = new StringTokenizer(br.readLine(), " ");
+
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
