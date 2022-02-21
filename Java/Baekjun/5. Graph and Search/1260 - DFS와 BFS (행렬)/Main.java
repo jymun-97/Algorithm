eef024f2561b48ae4363758d0b7e31b784c65ba0
jymun-97 <mjy0750@gmail.com>
2022-02-21 20:19:19 +0900

    add: solution of Boj_1260_DFS와 BFS (행렬)

--- Java/Baekjun/5. Graph and Search/1260 - DFS와 BFS (행렬)/Main.java
+++ Java/Baekjun/5. Graph and Search/1260 - DFS와 BFS (행렬)/Main.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int nVertex, nEdge, start;
