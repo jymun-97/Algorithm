a297f0394b06074a23c46dbda08f3af2b3bba4d1
jymun-97 <mjy0750@gmail.com>
2022-04-25 20:19:19 +0900

    add: solution of Pro_43238_입국심사

---
+++ Group Study/Month 6/week 2/Pro_43238_입국심사/Pro_43238.kt
@@ -0,0 +1,44 @@
+
+class Solution {
+    var n = 0
+    lateinit var times: IntArray
+
+    fun solution(n: Int, times: IntArray): Long {
+        this.n = n
+        this.times = times
+
+        var left = 1L
+        var right = 1_000_000_000L * 100_000L
+
+        var ans = 0L
+        while (left <= right) {
+            val mid = (left + right) / 2L
+
+            if (isPossible(mid)) {
+                ans = mid
+                right = mid - 1L
+            }
+            else left = mid + 1L
+        }
+
+        return ans
+    }
+
+    private fun isPossible(target: Long) : Boolean {
+        var sum = 0L
+
+        times.forEach { time ->
+            sum += target / time
+        }
+
+        return sum >= n
+    }
+}
+
+fun main() {
+    val s = Solution()
+    val n = 1000000000
+    val times = intArrayOf(100000)
+
+    print(s.solution(n, times))
+}
---
+++ Group Study/Month 6/week 2/Pro_62048_멀쩡한 사각형/Pro_62048.kt
@@ -0,0 +1,23 @@
+import java.io.*
+import java.util.*
+
+class Solution {
+    fun solution(w: Int, h: Int): Long {
+        var answer: Long = w * h.toLong()
+
+        val gcd = GCD(w, h)
+        val nw = w / gcd
+        val nh = h / gcd
+
+        answer -= (nw + nh - 1) * gcd
+
+        return answer
+    }
+    fun GCD(a: Int, b: Int): Long =
+        if (b == 0) a.toLong()
+        else GCD(b, a % b)
+}
+
+fun main() {
+
+}
