92cc2b62a5378b45bff1ac8790c48de791eba4d3
jymun-97 <mjy0750@gmail.com>
2022-05-10 20:19:19 +0900

    add: solution of Boj_12904_A와 B

---
+++ Kotlin/Boj_12904_A와 B/Boj_12904.kt
@@ -0,0 +1,25 @@
+var str = ""
+var target = ""
+
+fun input() = with(System.`in`.bufferedReader()) {
+    str = readLine()
+    target = readLine()
+}
+
+fun solve() {
+    while (str.length < target.length) {
+        val last = target.last()
+        target = target.dropLast(1)
+
+        if (last == 'B') target = target.reversed()
+    }
+    println(
+        if (str == target) 1
+        else 0
+    )
+}
+
+fun main() {
+    input()
+    solve()
+}
