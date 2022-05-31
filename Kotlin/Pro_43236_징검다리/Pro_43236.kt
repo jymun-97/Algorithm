3f98635b45c0782cf13fe93c8cc040b6e5a0c19a
jymun-97 <mjy0750@gmail.com>
2022-05-31 20:19:19 +0900

    add: solution of Pro_43236_징검다리

---
+++ Kotlin/Pro_43236_징검다리/Pro_43236.kt
@@ -0,0 +1,31 @@
+class Solution {
+    fun solution(distance: Int, rocks: IntArray, n: Int): Int {
+        rocks.apply {
+            plus(distance)
+            sort()
+        }
+
+        var ans = 0
+        var left = 0
+        var right = distance
+
+        while (left <= right) {
+            val mid = (left + right) / 2
+
+            var target = 0
+            var count = 0
+            rocks.forEach {
+                if (it - target < mid) count++
+                else target = it
+            }
+
+            if (count > n) right = mid - 1
+            else {
+                ans = mid
+                left = mid + 1
+            }
+        }
+
+        return ans
+    }
+}
