66e2b2e44f7e25169b01b85c6a65a89104064e41
jymun-97 <mjy0750@gmail.com>
2022-06-14 20:19:19 +0900

    add: solution of Programmers 신고 결과 받기

---
+++ 모의 코딩테스트/2022 Kakao Blind/신고 결과 받기/App.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+
+        HashMap<String, Integer> index = new HashMap<>();
+        HashMap<String, HashSet<String>> set = new HashMap<>(); // 나를 신고한 사람들 목록
+
+        for (int i = 0; i < id_list.length; i++) {
+            index.put(id_list[i], i);
+            set.put(id_list[i], new HashSet<>());
+        }
+
+        for (String info : report) {
+            StringTokenizer st = new StringTokenizer(info);
+            String from = st.nextToken();
+            String to = st.nextToken();
+
+            set.get(to).add(from);
+        }
+
+        for (int i = 0; i < id_list.length; i++) {
+            if (set.get(id_list[i]).size() >= k) {
+                for (String id : set.get(id_list[i])) {
+                    answer[index.get(id)]++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
