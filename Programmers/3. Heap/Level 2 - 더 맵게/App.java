cf9653fb1aaabeb65e1c654e68789d476f379fc5
jymun-97 <mjy0750@gmail.com>
2022-06-30 00:50:50 +0900

    add: solution of Programmers 더 맵게

---
+++ Programmers/3. Heap/Level 2 - 더 맵게/App.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        for (int s : scoville) que.add(s);
+
+        while (!que.isEmpty()) {
+            if (que.peek() >= K) break;
+            if (que.size() == 1) {
+                answer = -1;
+                break;
+            }
+            int mix = que.poll() + que.poll() * 2;
+            que.add(mix);
+            answer++;
+        }
+        return answer;
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
