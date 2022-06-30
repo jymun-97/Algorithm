384beba912261df20e40b6cc7d69ea231d858b7c
jymun-97 <mjy0750@gmail.com>
2022-06-30 22:15:33 +0900

    add: solution of K번째 수 1300

--- Java/Baekjun/2. Binary Search/K번째 수 1300/Boj_1300.java
+++ Java/Baekjun/2. Binary Search/K번째 수 1300/Boj_1300.java
@@ -1,7 +1,6 @@
 import java.io.*;
 
 public class Boj_1300 {
-
     static int n, k;
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
