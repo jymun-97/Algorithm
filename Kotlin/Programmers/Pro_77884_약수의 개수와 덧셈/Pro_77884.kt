b077c7e00f56afd82f15334baaf07647b4df3f14
jymun-97 <mjy0750@gmail.com>
2022-07-06 10:04:39 +0900

    modify: solution of Pro_77884_약수의 개수와 덧셈

---
+++ Kotlin/Programmers/Pro_77884_약수의 개수와 덧셈/Pro_77884.kt
@@ -0,0 +1,8 @@
+import kotlin.math.sqrt
+
+class Solution {
+    fun solution(left: Int, right: Int): Int =
+        (left .. right).fold(0) { ans, num ->
+            if (sqrt(num + 0.0) % 1 == 0.0) ans - num else ans + num
+        }
+}
