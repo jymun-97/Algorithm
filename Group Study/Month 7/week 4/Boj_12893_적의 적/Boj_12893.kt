2df416fd8f94067a2629a180c464e05ed01f0a1c
jymun-97 <mjy0750@gmail.com>
2022-07-28 20:23:02 +0900

    add: solution of Boj_12893_적의 적

---
+++ Group Study/Month 7/week 4/Boj_12893_적의 적/Boj_12893.kt
@@ -0,0 +1,69 @@
+import java.util.*
+
+var n = 0
+var m = 0
+lateinit var people : IntArray
+lateinit var graph : Array<ArrayList<Int>>
+
+fun input() = with(System.`in`.bufferedReader()) {
+    readLine().split(" ").map { it.toInt() }.also {
+        n = it[0]
+        m = it[1]
+    }
+    people = IntArray(n + 1) { -1 }
+    graph = Array(n + 1) { arrayListOf() }
+
+    repeat(m) {
+        val (from, to) = readLine().split(" ").map { it.toInt() }
+
+        graph[from].add(to)
+        graph[to].add(from)
+    }
+}
+
+fun solve() {
+
+    for (i in 1 .. n) {
+        if (people[i] == -1) {
+            val response = bfs(i)
+
+            if (!response) {
+                println(0)
+                return
+            }
+        }
+    }
+
+    println(1)
+}
+
+fun bfs(start: Int) : Boolean {
+    val que = LinkedList<Int>()
+
+    que.add(start)
+    people[start] = 0
+
+    while (que.isNotEmpty()) {
+        val from = que.poll()
+
+        graph[from].forEach { to ->
+            when (people[to]) {
+                -1 -> {
+                    que.add(to)
+                    people[to] = 1 - people[from]
+                }
+
+                people[from] -> return false
+
+                else -> return@forEach
+            }
+        }
+    }
+
+    return true
+}
+
+fun main() {
+    input()
+    solve()
+}
