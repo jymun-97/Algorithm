4770c8232a62d168aa6a0cdbb92fb206e0091050
jymun-97 <mjy0750@gmail.com>
2022-08-18 02:35:37 +0900

    refactor: solution of Pro_12925_문자열을 정수로 바꾸기

--- Kotlin/Programmers/Pro_12925_문자열을 정수로 바꾸기/Pro_12925.kt
+++ Kotlin/Programmers/Pro_12925_문자열을 정수로 바꾸기/Pro_12925.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(s: String) : Int = when {
-        s[0] == '+' -> s.substring(1).toInt()
-
-        s[0] == '-' -> s.substring(1).toInt() * -1
-
-        else -> s.toInt()
-    }
+    fun solution(s: String) : Int = s.toInt()
 }
