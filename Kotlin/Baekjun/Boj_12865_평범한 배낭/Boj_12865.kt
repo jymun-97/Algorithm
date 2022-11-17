df0fc208dff881d5d5a8c0573cf91e408e028068
jymun-97 <mjy0750@gmail.com>
2022-11-17 12:02:08 +0900

    todo: Boj_12865_평범한 배낭

--- Kotlin/Baekjun/Boj_12865_평범한 배낭/Boj_12865.kt
+++ Kotlin/Baekjun/Boj_12865_평범한 배낭/Boj_12865.kt
@@ -1,30 +1,41 @@
 import java.util.*
 import kotlin.math.max
 
-data class Info(
-    val weight: Int,
-    val value: Int
-)
 var n = 0
+var k = 0
 var ans = 0
-var limit = 0
-lateinit var arr: List<Info>
-lateinit var dp: IntArray
+lateinit var weights: IntArray
+lateinit var values: IntArray
+lateinit var dp: Array<IntArray>
 
 fun input() = with(System.`in`.bufferedReader()) {
     StringTokenizer(readLine()).apply {
         n = nextToken().toInt()
-        limit = nextToken().toInt()
+        k = nextToken().toInt()
     }
-    dp = IntArray(limit + 1)
-    arr = List(n) {
+    weights = IntArray(n)
+    values = IntArray(n)
+    repeat(4) {
         val (weight, value) = readLine().split(" ").map { it.toInt() }
-        Info(weight, value)
-    }.sortedWith(compareBy( { it.weight }, { -it.value }))
+        weights[it] = weight
+        values[it] = value
+    }
+    dp = Array(n) { IntArray(k + 1) { -1 } }
 }
 
 fun solve() {
-    for (i in 0 until arr[0].weight) dp[i]
+    println(knapsack(n - 1, k))
+}
+
+fun knapsack(i: Int, weight: Int): Int {
+    if (weight <= 0 || i < 0) return 0
+    if (dp[i][weight] != -1) return dp[i][weight]
+
+    dp[i][weight] = if (weights[i] <= weight) max(
+        knapsack(i - 1, weight - weights[i]) + values[i],
+        knapsack(i - 1, weight)
+    ) else knapsack(i - 1, weight)
+    return dp[i][weight]
 }
 
 fun main() {
