9459c29362a924fddb8d1d676ac67e56613792c7
jymun-97 <mjy0750@gmail.com>
2022-11-21 16:11:47 +0900

    add: solution of Boj_2042_구간 합 구하기

--- Kotlin/Baekjun/Boj_2042_구간 합 구하기/Boj_2042.kt
+++ Kotlin/Baekjun/Boj_2042_구간 합 구하기/Boj_2042.kt
@@ -5,6 +5,7 @@ var m = 0
 var k = 0
 lateinit var nums: LongArray
 lateinit var commands: Array<String>
+lateinit var tree: LongArray
 
 fun input() = with(System.`in`.bufferedReader()) {
     StringTokenizer(readLine()).apply {
@@ -14,21 +15,58 @@ fun input() = with(System.`in`.bufferedReader()) {
     }
     nums = LongArray(n + 1) { if (it == 0) 0 else readLine().toLong() }
     commands = Array(m + k) { readLine() }
+    tree = LongArray(4 * n + 1)
 }
 
 fun solve() {
+    init(1, n, 1)
     val sb = StringBuilder()
 
     commands.forEach { command ->
         val (a, b, c) = command.split(" ").map { it.toLong() }
         if (a == 1L) {
-
+            update(1, n, 1, b.toInt(), c - nums[b.toInt()])
+            nums[b.toInt()] = c
         } else {
-
+            sb.append(
+                sum(1, n, 1, b.toInt(), c.toInt())
+            ).append('\n')
         }
     }
+    print(sb)
+}
 
-    println(sb)
+fun init(start: Int, end: Int, node: Int): Long {
+    tree[node] = when (start) {
+        end -> nums[start]
+        else -> {
+            val mid = (start + end) / 2
+            init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1)
+        }
+    }
+    return tree[node]
+}
+
+fun sum(start: Int, end: Int, node: Int, left: Int, right: Int): Long = when {
+    start > right || end < left -> 0
+
+    start >= left && end <= right -> tree[node]
+
+    else -> {
+        val mid = (start + end) / 2
+        sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right)
+    }
+}
+
+fun update(start: Int, end: Int, node: Int, index: Int, diff: Long)  {
+    if (index !in start..end) return
+
+    tree[node] += diff
+    if (start == end) return
+
+    val mid = (start + end) / 2
+    update(start, mid, node * 2, index, diff)
+    update(mid + 1, end, node * 2 + 1, index, diff)
 }
 
 fun main() {
