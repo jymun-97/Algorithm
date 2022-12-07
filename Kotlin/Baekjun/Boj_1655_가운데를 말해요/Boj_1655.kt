95ca49357e2d75b82a5755f0724b8e8a00942126
jymun-97 <mjy0750@gmail.com>
2022-12-07 12:25:15 +0900

    add: solution of Boj_1655_가운데를 말해요

--- Kotlin/Baekjun/Boj_1655_가운데를 말해요/Boj_1655.kt
+++ Kotlin/Baekjun/Boj_1655_가운데를 말해요/Boj_1655.kt
@@ -1,81 +1,32 @@
 import java.util.*
-import kotlin.math.min
 
-val builder = StringBuilder()
-
-var n = 0
-lateinit var root: Node
-lateinit var values: IntArray
-
-data class Node(
-    var parent: Node? = null,
-    var left: Node? = null,
-    var right: Node? = null,
-    val value: Int,
-    var count: Int = 0,
-    var index: Int = 0,
-) {
-//    val count: Int
-//        get() = (left?.count ?: 0) + (right?.count ?: 0)
-//
-//    val index: Int
-//        get() = (left?.count ?: 0) +
-//                if (parent != null && parent!!.value <= value) parent!!.index + 1 else 0
-}
+val nums = arrayListOf<Int>()
+lateinit var inputNumbers: List<Int>
 
 fun input() = with(System.`in`.bufferedReader()) {
-    n = readLine().toInt()
-    values = IntArray(n) { readLine().toInt() }
+    val n = readLine().toInt()
+    inputNumbers = List(n) { readLine().toInt() }
 }
 
 fun solve() {
-    root = Node(value = values[0])
-//    builder.append(root.value).append('\n')
+    val builder = StringBuilder()
+    val left = PriorityQueue<Int>() { i1, i2 -> i2 - i1 }
+    val right = PriorityQueue<Int>()
 
-    for (i in 1 until values.size) {
-        insert(root, values[i])
+    inputNumbers.forEach {
+        when {
+            left.size <= right.size -> left.add(it)
 
-//        val target = (i + 1) / 2
-//        builder.append(
-//            if (i % 2 == 0) find(root, target).value
-//            else min(
-//                find(root, target).value,
-//                find(root, target - 1).value
-//            )
-//        ).append('\n')
-    }
-    val ans = find(root, 0)
-    println(ans)
-}
-
-fun insert(parent: Node, value: Int): Unit = when {
-    value <= parent.value -> {
-        parent.count++
-        parent.index++
-
-        parent.left?.let {
-            insert(it, value)
-        } ?: run {
-            parent.left = Node(parent = parent, value = value)
+            else -> right.add(it)
         }
-    }
-    else -> {
-        parent.count++
-
-        parent.right?.let {
-            insert(it, value)
-        } ?: run {
-            parent.right = Node(parent = parent, value = value, index = parent.index + 1)
+        if (left.isNotEmpty() && right.isNotEmpty() && left.peek() > right.peek()) {
+            left.add(right.poll()).also {
+                right.add(left.poll())
+            }
         }
+        builder.append(left.peek()).append('\n')
     }
-}
-
-fun find(node: Node, target: Int): Node = when {
-    node.index == target -> node
-
-    node.index > target -> find(node.left!!, target)
-
-    else -> find(node.right!!, target)
+    println(builder)
 }
 
 fun main() {
