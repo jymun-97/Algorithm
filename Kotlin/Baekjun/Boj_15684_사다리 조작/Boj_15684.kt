02ba10fdd654a14acdd250988687acb9e9d9f494
jymun-97 <mjy0750@gmail.com>
2022-12-12 20:21:34 +0900

    add: solution of Boj_15684_사다리 조작

--- Kotlin/Baekjun/Boj_15684_사다리 조작/Boj_15684.kt
+++ Kotlin/Baekjun/Boj_15684_사다리 조작/Boj_15684.kt
@@ -1,76 +1,60 @@
-import kotlin.collections.HashSet
 
 var n = 0
 var m = 0
 var k = 0
-var min = 4
-lateinit var graph: Array<HashSet<Pair<Int, Int>>>
+var ans = Int.MAX_VALUE
+lateinit var isLinked: Array<BooleanArray>
 
 fun input() = with(System.`in`.bufferedReader()) {
-    readLine().split(" ").map { it.toInt() }.apply {
-        n = this[2]
-        m = this[0]
-        k = this[1]
+    readLine().split(" ").map { it.toInt() }.let {
+        n = it[2]
+        m = it[0]
+        k = it[1]
     }
-    graph = Array(n + 1) { hashSetOf() }
-
+    isLinked = Array(n + 1) { BooleanArray(m + 1) }
     repeat(k) {
         val (row, col) = readLine().split(" ").map { it.toInt() }
-        link(row, col)
+        isLinked[row][col] = true
     }
 }
 
 fun solve() {
     dfs(1, 1, 0, true)
-    println(
-        if (min == 4) -1
-        else min
-    )
+    println(if (ans == Int.MAX_VALUE) -1 else ans)
 }
 
-fun link(row: Int, col: Int) = graph[row].apply {
-    add(col to col + 1)
-    add(col + 1 to col)
-}
-
-fun unlink(row: Int, col: Int) = graph[row].apply {
-    remove(col to col + 1)
-    remove(col + 1 to col)
-}
-
-fun goDown(row: Int, col: Int, flag: Boolean): Int = when {
+fun moveDown(row: Int, col: Int, flag: Boolean): Int = when {
     row > n -> col
 
-    !flag && isLinked(row, col, -1) -> goDown(row, col - 1, true)
+    !flag && isLinked[row][col - 1] -> moveDown(row, col - 1, true)
 
-    !flag && isLinked(row, col, 1) -> goDown(row, col + 1, true)
+    !flag && isLinked[row][col] -> moveDown(row, col + 1, true)
 
-    else -> goDown(row + 1, col, false)
+    col > m -> moveDown(row + 1, 1, false)
+
+    else -> moveDown(row + 1, col, false)
 }
 
-fun isLinked(row: Int, col: Int) = graph[row].contains(col to col - 1) || graph[row].contains(col to col + 1)
-fun isLinked(row: Int, col: Int, dir: Int) = graph[row].contains(col to col + dir)
+fun dfs(row: Int, col: Int, count: Int, flag: Boolean) : Unit = when {
+    count >= ans || count > 3 -> {}
 
-fun dfs(row: Int, col: Int, count: Int, flag: Boolean) {
-    when {
-        row > n || count >= min -> return
+    flag && isValid() -> ans = count
 
-        flag && isFinish() -> min = count
+    row > n -> {}
 
-        col > m -> dfs(row + 1, 1, count, false)
+    col == m -> dfs(row + 1, 1, count, flag)
 
-        !isLinked(row, col) && !isLinked(row, col + 1) -> {
-            dfs(row, col + 1, count, false)
-            link(row, col)
-            dfs(row, col + 1, count + 1, true)
-            unlink(row, col)
-        }
+    isLinked[row][col] || isLinked[row][col + 1] -> dfs(row, col + 1, count, false)
 
-        else -> dfs(row, col + 1, count, false)
+    else -> {
+        dfs(row, col + 1, count, false)
+        isLinked[row][col] = true
+        dfs(row, col + 1, count + 1, true)
+        isLinked[row][col] = false
     }
 }
 
-fun isFinish() = (1..m).all { it == goDown(1, it, false) }
+fun isValid() = (1..m).all { it == moveDown(1, it, false) }
 
 fun main() {
     input()
