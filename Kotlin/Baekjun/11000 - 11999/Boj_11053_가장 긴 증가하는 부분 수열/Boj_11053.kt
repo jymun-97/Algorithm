176efcc823b47d682942f6cb0d7701597f861ac1
jymun-97 <mjy0750@gmail.com>
2022-12-16 17:36:35 +0900

    add: solution of Boj_11053_가장 긴 증가하는 부분 수열

--- Kotlin/Baekjun/11000 - 11999/Boj_11053_가장 긴 증가하는 부분 수열/Boj_11053.kt
+++ Kotlin/Baekjun/11000 - 11999/Boj_11053_가장 긴 증가하는 부분 수열/Boj_11053.kt
@@ -3,29 +3,23 @@ import kotlin.math.max
 var n = 0
 var ans = 0
 lateinit var nums: List<Int>
-lateinit var visit: BooleanArray
+lateinit var dp: IntArray
 
 fun input() = with(System.`in`.bufferedReader()) {
     n = readLine().toInt()
     nums = readLine().split(" ").map { it.toInt() }
-    visit = BooleanArray(n)
+    dp = IntArray(n) { 1 }
 }
 
 fun solve() {
-    (0 until n).forEach { dfs(it, 1) }
-    println(ans)
-}
-
-fun dfs(from: Int, length: Int) {
-    visit[from] = true
-    if (ans >= length + n - from - 1) return
-
-    for (to in from + 1 until n) {
-        if (nums[from] < nums[to] && !visit[to]) {
-            ans = max(ans, length + 1)
-            dfs(to, length + 1)
+    for (i in 1 until n) {
+        for (j in 0 until i) {
+            if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1
+            }
         }
     }
+    println(dp.maxOf { it })
 }
 
 fun main() {
